78505e88ee1afee8ef3cc4850c55d208f771653d
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-03-03 16:01:32 -0600

    Create Scroll.java

---
+++ src/main/java/me/gamercoder215/calcgames/levelz/Scroll.java
@@ -0,0 +1,33 @@
+package me.gamercoder215.calcgames.levelz;
+
+/**
+ * Represents the scroll direction of a 2D Level.
+ */
+public enum Scroll {
+
+    /**
+     * No Scrolling
+     */
+    NONE,
+
+    /**
+     * Horizontal Scrolling moving left
+     */
+    HORIZONTAL_LEFT,
+
+    /**
+     * Horizontal Scrolling moving right
+     */
+    HORIZONTAL_RIGHT,
+
+    /**
+     * Vertical Scrolling moving up
+     */
+    VERTICAL_UP,
+
+    /**
+     * Vertical Scrolling moving down
+     */
+    VERTICAL_DOWN
+
+}
