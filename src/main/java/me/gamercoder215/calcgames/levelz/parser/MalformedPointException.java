29432a15faad083386b61226aad5f3b7b58909b4
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-03-03 16:01:24 -0600

    Draft Parser Exceptions

---
+++ src/main/java/me/gamercoder215/calcgames/levelz/parser/MalformedPointException.java
@@ -0,0 +1,27 @@
+package me.gamercoder215.calcgames.levelz.parser;
+
+/**
+ * Thrown when a point is malformed.
+ */
+public class MalformedPointException extends ParseException {
+
+    private static final long serialVersionUID = 3594456258251637068L;
+
+    /**
+     * Creates a new Malformed Point Exception.
+     * @param message Message
+     */
+    public MalformedPointException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new Malformed Point Exception.
+     * @param message Message
+     * @param cause Cause
+     */
+    public MalformedPointException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
