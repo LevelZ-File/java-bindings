e658a1182d31df85eece5f28d49a2f20f6971973
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-03-03 22:14:31 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,59 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
+
     java
+    `maven-publish`
+    jacoco
 }
 
 val jvm = JavaVersion.VERSION_11
+
+group = "me.gamercoder215.calcgames"
+version = "0.1.0"
 
 java {
     sourceCompatibility = jvm
     targetCompatibility = jvm
 }
 
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
 dependencies {
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
+    compileOnly(kotlin("stdlib"))
+    compileOnly("org.jetbrains:annotations:24.1.0")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        options.isDeprecation = false
+        options.isWarnings = false
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+        finalizedBy(jacocoTestReport)
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+
+        reports {
+            xml.required.set(true)
+            xml.outputLocation.set(layout.buildDirectory.file("jacoco.xml"))
+
+            html.required.set(true)
+            html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        }
+    }
 }
