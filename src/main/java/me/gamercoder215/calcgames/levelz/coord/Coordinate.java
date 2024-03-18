bda7240ef3ee2d5e9694b7ba61f514ff691a718b
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2024-03-17 20:20:54 -0500

    Create Coordinate#fromArray

--- src/main/java/me/gamercoder215/calcgames/levelz/coord/Coordinate.java
+++ src/main/java/me/gamercoder215/calcgames/levelz/coord/Coordinate.java
@@ -26,4 +26,16 @@ public interface Coordinate {
 
     int hashCode();
 
+    /**
+     * Creates a Coordinate from an array of integers.
+     * @param coords Coordinate Array
+     * @return A {@linkplain Coordinate2D 2D} or {@linkplain Coordinate3D 3D} Coordinate, depending on array size
+     */
+    static Coordinate fromArray(int[] coords) {
+        if (coords.length == 2) return new Coordinate2D(coords[0], coords[1]);
+        if (coords.length == 3) return new Coordinate3D(coords[0], coords[1], coords[2]);
+
+        throw new IllegalArgumentException("Invalid Coordinate Length");
+    }
+
 }
