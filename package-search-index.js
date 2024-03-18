d8bb77951b87b31e838ba541f1d84e607aa59878
GitHub Action <action@github.com>
2024-03-18 01:28:40 +0000

    Update JavaDocs (e115af3)

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.gamercoder215.calcgames.levelz"},{"l":"me.gamercoder215.calcgames.levelz.coord"},{"l":"me.gamercoder215.calcgames.levelz.parser"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.gamercoder215.calcgames.levelz"},{"l":"me.gamercoder215.calcgames.levelz.builder"},{"l":"me.gamercoder215.calcgames.levelz.coord"},{"l":"me.gamercoder215.calcgames.levelz.parser"}];updateSearchResults();
