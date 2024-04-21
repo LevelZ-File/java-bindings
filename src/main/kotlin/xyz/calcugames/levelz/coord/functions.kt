824843b519f58cf63802059bfbe34da5841a9734
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2024-04-20 21:47:00 -0500

    Fix JVM Clash

--- src/main/kotlin/xyz/calcugames/levelz/coord/functions.kt
+++ src/main/kotlin/xyz/calcugames/levelz/coord/functions.kt
@@ -53,23 +53,7 @@ fun coordinate2D(xy: DoubleArray) = Coordinate2D(xy[0], xy[1])
  * @param y The supplier to the y value.
  * @return The 2D coordinate.
  */
-fun coordinate2D(x: () -> Int, y: () -> Int): Coordinate2D = Coordinate2D(x(), y())
-
-/**
- * Creates a 2D coordinate.
- * @param x The supplier to the x value.
- * @param y The supplier to the y value.
- * @return The 2D coordinate.
- */
-fun coordinate2D(x: () -> Float, y: () -> Float): Coordinate2D = Coordinate2D(x().toDouble(), y().toDouble())
-
-/**
- * Creates a 2D coordinate.
- * @param x The supplier to the x value.
- * @param y The supplier to the y value.
- * @return The 2D coordinate.
- */
-fun coordinate2D(x: () -> Double, y: () -> Double): Coordinate2D = Coordinate2D(x(), y())
+fun coordinate2D(x: () -> Number, y: () -> Number): Coordinate2D = Coordinate2D(x().toDouble(), y().toDouble())
 
 // Coordinate3D
 
@@ -128,22 +112,4 @@ fun coordinate3D(xyz: DoubleArray) = Coordinate3D(xyz[0], xyz[1], xyz[2])
  * @param z The supplier to the z value.
  * @return The 3D coordinate.
  */
-fun coordinate3D(x: () -> Int, y: () -> Int, z: () -> Int): Coordinate3D = Coordinate3D(x(), y(), z())
-
-/**
- * Creates a 3D coordinate.
- * @param x The supplier to the x value.
- * @param y The supplier to the y value.
- * @param z The supplier to the z value.
- * @return The 3D coordinate.
- */
-fun coordinate3D(x: () -> Float, y: () -> Float, z: () -> Float): Coordinate3D = Coordinate3D(x().toDouble(), y().toDouble(), z().toDouble())
-
-/**
- * Creates a 3D coordinate.
- * @param x The supplier to the x value.
- * @param y The supplier to the y value.
- * @param z The supplier to the z value.
- * @return The 3D coordinate.
- */
-fun coordinate3D(x: () -> Double, y: () -> Double, z: () -> Double): Coordinate3D = Coordinate3D(x(), y(), z())
+fun coordinate3D(x: () -> Number, y: () -> Number, z: () -> Number): Coordinate3D = Coordinate3D(x().toDouble(), y().toDouble(), z().toDouble())
